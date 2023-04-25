bb803416a0c825f5563d3e33a33919d67ee172cf
Ciro Amarilla <71462009+cjamarilla@users.noreply.github.com>
2023-04-25 18:08:56 -0300

    finalizado ejercicio 6

--- MI-6/main.cpp
+++ MI-6/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
 using namespace std;
-
+int fecha, fecha2;
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout<<"Ingrese un fecha con el formato [AAAAMMDD]: ";
+    cin>>fecha;
+    cout<<"Ingrese un fecha con el formato [AAAAMMDD]: ";
+    cin>>fecha2;
+    if(fecha>fecha2){
+        cout<<"La fecha mayor es: "<<fecha<<endl;
+    }else if(fecha<fecha2){
+            cout<<"La fecha mayor es: "<<fecha2<<endl;
+    }else{
+                cout<<"Las fechas son iguales";
+    }
+
     return 0;
 }
