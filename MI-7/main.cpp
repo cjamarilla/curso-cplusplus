6810482352aaa20172868f63c11b92a7b2e02929
Ciro Joel Amarilla <71462009+cjamarilla@users.noreply.github.com>
2023-04-27 16:32:55 -0300

    Finalizado ejercicio 7

--- MI-7/main.cpp
+++ MI-7/main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int l1,l2,l3;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    do{
+        cout<<"Ingrese 3 datos: "; cin>>l1>>l2>>l3;
+        if(l1<=0 || l2<=0 || l3<=0){
+            cout<<"Recuerde, que los valores ingresados tienen que ser mayor que cero"<<endl;
+        }
+    }while(l1<=0 || l2<=0 || l3<=0);
+
+    if((l1 + l2 > l3)||(l2 + l3 > l1) || (l1 + l3 > l2)){
+        cout<<"Forman un Triangulo";
+    }
+
+
     return 0;
 }
