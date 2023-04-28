8592564f03dae53c76ea8e6829743ba51aa55b7b
Ciro Joel Amarilla <71462009+cjamarilla@users.noreply.github.com>
2023-04-27 21:27:11 -0300

    end 11-12-13-14

---
+++ MI-15/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void
+
+int main(){
+    int tipo_infraccion,valor_multa;
+    char motivo[50];
+    char gravedad[10];
+
+    for(int i=0; i<=20; i++){
+
+
+
+
+    }
+    return 0;
+}
